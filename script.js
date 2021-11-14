f7f3971d94ddd15f5f3c5369e1c4b9017a9597c2
fishball <hychanbn1009@gmail.com>
2021-11-14 18:32:14 +0800

    adding Loading feature

--- script.js
+++ script.js
@@ -5,23 +5,33 @@ api_link=`api.openweathermap.org/data/2.5/weather?q=London&appid=${api_key}`
 
 // const cityName = document.getElementById('cityName');
 async function getWeather(city){
+    displaycityName.innerHTML='loading...'
   const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0f4cfc1673ca8fbe1c82881b21b24159`, {mode: 'cors'})
   const tempData = await response.json().then(response=>{
     if(response.cod===200){
     console.log(response)
-    console.log(`you are in ${response.name}`)
-    console.log(`max temp is ${response.main.temp_max}`)
-    console.log(`min temp is ${response.main.temp_min}`)
-    console.log(`your feeling temp is ${response.main.temp}`)
-    console.log(`humidity is ${response.main.humidity}`)
-    cityName.innerHTML=response.name
+    const location =`you are in ${response.name}`
+    const maxTemp = `max temp is ${response.main.temp_max}`
+    const minTemp = `min temp is ${response.main.temp_min}`
+    const feelTemp = `your feeling temp is ${response.main.temp}`
+    const humidity = `humidity is ${response.main.humidity}`
+    displaycityName.innerHTML=location
+    displaymaxTemp.innerHTML=maxTemp
+    displayminTemp.innerHTML=minTemp
+    displayfeelTemp.innerHTML=feelTemp
+    displayHumidity.innerHTML=humidity
     }else{
-        console.log('We cant find that place!')
+        displaycityName.innerHTML='We cant find that place!'
     }
   })
 }
 
 function Submit_CityName(){
     const city=document.getElementById('cityName').value
+    displaycityName.innerHTML=''
+    displaymaxTemp.innerHTML=''
+    displayminTemp.innerHTML=''
+    displayfeelTemp.innerHTML=''
+    displayHumidity.innerHTML=''
     getWeather(city)
 }
