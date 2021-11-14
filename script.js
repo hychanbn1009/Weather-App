13a666349af812f656ce7477b3c9ea41649507e8
fishball <hychanbn1009@gmail.com>
2021-11-14 18:14:01 +0800

    update response

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 api_key='0f4cfc1673ca8fbe1c82881b21b24159'
-city='London'
+// city='London'
 api_link=`api.openweathermap.org/data/2.5/weather?q=London&appid=${api_key}`
 
 
-const cityName = document.querySelector('cityName');
-async function getWeather(){
-  const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=0f4cfc1673ca8fbe1c82881b21b24159', {mode: 'cors'})
-  console.log(response)
-  const tempData = await response.json()
-  console.log(tempData)
+// const cityName = document.getElementById('cityName');
+async function getWeather(city){
+  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0f4cfc1673ca8fbe1c82881b21b24159`, {mode: 'cors'})
+  const tempData = await response.json().then(response=>{
+    if(response.cod===200){
+    console.log(response)
+    console.log(`you are in ${response.name}`)
+    console.log(`max temp is ${response.main.temp_max}`)
+    console.log(`min temp is ${response.main.temp_min}`)
+    console.log(`your feeling temp is ${response.main.temp}`)
+    console.log(`humidity is ${response.main.humidity}`)
+    cityName.innerHTML=response.name
+    }else{
+        console.log('We cant find that place!')
+    }
+  })
 }
-getWeather()
+
+function Submit_CityName(){
+    const city=document.getElementById('cityName').value
+    getWeather(city)
+}
